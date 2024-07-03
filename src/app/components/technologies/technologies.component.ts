9fe99cbd67175386db724b4b3bb823912dcf0826
Simone Bagordo <simonsba1998@gmail.com>
2024-07-03 15:44:47 +0200

    technologies logo responsive design

--- src/app/components/technologies/technologies.component.ts
+++ src/app/components/technologies/technologies.component.ts
@@ -12,4 +12,15 @@ export class TechnologiesComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  logos = [
+    { src: '../../../assets/image/angular-logo.png', alt: 'Angular' },
+    { src: '../../../assets/image/react-logo.png', alt: 'React' },
+    { src: '../../../assets/image/java-logo.png', alt: 'Java' },
+    { src: '../../../assets/image/bootstrap-logo.png', alt: 'Bootstrap' },
+    { src: '../../../assets/image/material-logo.png', alt: 'Material' },
+    { src: '../../../assets/image/rxjs-logo.png', alt: 'RxJS' },
+    { src: '../../../assets/image/spring-logo.png', alt: 'Spring' },
+    { src: '../../../assets/image/sql-logo.png', alt: 'SQL' },
+  ];
+
 }
