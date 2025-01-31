28a5e27cf7e878652b089f4b7b6b8a35b9e3cc09
Simone Bagordo <simonsba1998@gmail.com>
2025-01-31 16:07:11 +0100

    Add fireworks animations

--- src/app/components/guess-number-game/guess-number-game.component.ts
+++ src/app/components/guess-number-game/guess-number-game.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { Fireworks } from 'fireworks-js';
 
 @Component({
   selector: 'app-guess-number-game',
   templateUrl: './guess-number-game.component.html',
   styleUrls: ['./guess-number-game.component.css'],
 })
-export class GuessNumberGameComponent implements OnInit {
+export class GuessNumberGameComponent implements OnInit, AfterViewInit {
+  @ViewChild('fireworksCanvas', { static: false }) fireworksCanvas?: ElementRef;
   numeroSegreto: number = Math.floor(Math.random() * 100) + 1;
   numeroInserito? = new FormControl(null, [Validators.required]);
   messaggio: string = '';
   coloreMessaggio: string = 'black';
   vincita: boolean = false;
 
+  private fireworks?: Fireworks;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    if (this.fireworksCanvas) {
+      this.fireworks = new Fireworks(this.fireworksCanvas.nativeElement, {
+        autoresize: true,
+        opacity: 0.7
+      });
+    }
+  }
 
   checkNumber() {
     if (isNaN(this.numeroInserito?.value)) {
@@ -25,6 +38,10 @@ export class GuessNumberGameComponent implements OnInit {
     }
 
     if (this.numeroInserito?.value === this.numeroSegreto) {
+      this.fireworks?.start();
+      setTimeout(() => {
+        this.fireworks?.stop();
+      }, 5000);
       this.messaggio = '🎉 Complimenti! Hai indovinato!';
       this.vincita = true;
       this.coloreMessaggio = 'green';
@@ -39,6 +56,7 @@ export class GuessNumberGameComponent implements OnInit {
 
   restartGame() {
     this.vincita = false;
+    this.fireworks?.stop();
     this.numeroInserito?.setValue(undefined);
     this.numeroSegreto = Math.floor(Math.random() * 100) + 1;
   }
