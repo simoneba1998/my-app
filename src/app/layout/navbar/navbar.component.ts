476276b113b72d351daa50487e6a2cfb9593966a
Simone Bagordo <simonsba1998@gmail.com>
2025-02-04 15:12:06 +0100

    fix navbar responsive

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
+  isMenuOpen = false; // Stato del menu
 
-  constructor() { }
-
-  ngOnInit(): void {
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen; // Alterna true/false
   }
-
 }
