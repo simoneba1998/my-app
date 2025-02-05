caadea5b6047c6e93f6353e3fbd4b4ab8d1d43f1
Simone Bagordo <simonsba1998@gmail.com>
2025-02-05 15:52:55 +0100

    fix image path

--- src/app/Pages/Technologies/Technologies.component.ts
+++ src/app/Pages/Technologies/Technologies.component.ts
@@ -13,14 +13,14 @@ export class TechnologiesComponent implements OnInit {
   }
 
   logos = [
-    { src: '../../../assets/image/angular-logo.png', alt: 'ANGULAR' },
-    { src: '../../../assets/image/react-logo.png', alt: 'REACT' },
-    { src: '../../../assets/image/java-logo.png', alt: 'JAVA' },
-    { src: '../../../assets/image/bootstrap-logo.png', alt: 'BOOTSTRAP' },
-    { src: '../../../assets/image/material-logo.png', alt: 'MATERIAL' },
-    { src: '../../../assets/image/rxjs-logo.png', alt: 'RXJS' },
-    { src: '../../../assets/image/spring-logo.png', alt: 'SPRING' },
-    { src: '../../../assets/image/sql-logo.png', alt: 'SQL' },
+    { src: '/my-app/assets/image/angular-logo.png', alt: 'ANGULAR' },
+    { src: '/my-app/assets/image/react-logo.png', alt: 'REACT' },
+    { src: '/my-app/assets/image/java-logo.png', alt: 'JAVA' },
+    { src: '/my-app/assets/image/bootstrap-logo.png', alt: 'BOOTSTRAP' },
+    { src: '/my-app/assets/image/material-logo.png', alt: 'MATERIAL' },
+    { src: '/my-app/assets/image/rxjs-logo.png', alt: 'RXJS' },
+    { src: '/my-app/assets/image/spring-logo.png', alt: 'SPRING' },
+    { src: '/my-app/assets/image/sql-logo.png', alt: 'SQL' },
   ];
 
 }
